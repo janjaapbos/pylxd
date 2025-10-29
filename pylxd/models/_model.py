f985de863d81467a9ce3d28f03075371aafbdc09
Janjaap Bos <janjaap.bos@capitar.com>
2025-10-29 21:43:25 +0100

    Make model save() and delete() wait True

--- pylxd/models/_model.py
+++ pylxd/models/_model.py
@@ -226,7 +226,7 @@ class Model(metaclass=ModelType):
         """Reset the object from the server."""
         return self.sync(rollback=True)
 
-    def save(self, wait=False):
+    def save(self, wait=True):
         """Save data to the server.
 
         This method should write the new data to the server via marshalling.
@@ -240,7 +240,7 @@ class Model(metaclass=ModelType):
             self.client.operations.wait_for_operation(response.json()["operation"])
         self.__dirty__.clear()
 
-    def delete(self, wait=False):
+    def delete(self, wait=True):
         """Delete an object from the server."""
         response = self.api.delete()
 
